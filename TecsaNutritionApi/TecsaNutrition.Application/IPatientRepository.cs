6973fd9ef3afa53d0ef8ed00098d65ae99f8938b
Nakamura16 <andre.massao.nakamura@gmail.com>
2025-06-04 22:27:18 -0300

    Created Respository class and application project

---
+++ TecsaNutritionApi/TecsaNutrition.Application/IPatientRepository.cs
@@ -0,0 +1,10 @@
+﻿using TecsaNutrition.Models;
+using TecsaNutrition.Models.ApiHelpers;
+
+namespace TecsaNutrition.Application;
+
+public interface IPatientRepository
+{
+    Task<Patient> GetPatientById(int id);
+    Task<OperationResult> CreatePatient(Patient patient);
+}
---
+++ TecsaNutritionApi/TecsaNutrition.Data/Repository/PatientRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.AccessControl;
+using TecsaNutrition.Application;
+using TecsaNutrition.Models;
+using TecsaNutrition.Models.ApiHelpers;
+
+namespace TecsaNutrition.Data.Repository;
+
+public class PatientRepository : IPatientRepository
+{
+    private readonly TecsaNutritionContext context;
+
+    public PatientRepository(TecsaNutritionContext context)
+    {
+        this.context = context;
+    }
+
+    public Task<OperationResult> CreatePatient(Patient patient)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Patient> GetPatientById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
