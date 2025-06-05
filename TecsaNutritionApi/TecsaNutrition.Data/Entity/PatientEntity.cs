4cd12fdcf851a2917c92d1ab50d7eb0d748457b8
Nakamura16 <andre.massao.nakamura@gmail.com>
2025-06-04 22:19:07 -0300

    Creating Data project and DbContext

---
+++ TecsaNutritionApi/TecsaNutrition.Data/Entity/PatientEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace TecsaNutrition.Data.Entity;
+
+public class PatientEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Gender { get; set; }
+    public double Weight { get; set; }
+    public int HeightInCentimeters { get; set; }
+}
---
+++ TecsaNutritionApi/TecsaNutrition.Data/TecsaNutritionContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TecsaNutrition.Data.Entity;
+
+namespace TecsaNutrition.Data;
+
+public class TecsaNutritionContext : DbContext
+{
+    public TecsaNutritionContext(DbContextOptions<TecsaNutritionContext> options) : base(options) { }
+
+    public DbSet<PatientEntity> Patients { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
