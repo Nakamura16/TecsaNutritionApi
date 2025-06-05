3d8256e24520e15f841a911e1f70db1b419cb1e1
Nakamura16 <andre.massao.nakamura@gmail.com>
2025-06-04 22:12:41 -0300

    Created new Models project and added PatientModel

---
+++ TecsaNutritionApi/TecsaNutrition.Models/Patient.cs
@@ -0,0 +1,34 @@
+﻿namespace TecsaNutrition.Models;
+
+public class Patient
+{
+    public string Name{ get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public Gender Gender { get; set; }
+    public double Weight { get; set; }
+    public int HeightInCentimeters { get; set; }
+
+    public Patient(
+        string name,
+        string email,
+        string phoneNumber,
+        Gender gender,
+        double weight,
+        int heightInCentimeters)
+    {
+        Name = name;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Gender = gender;
+        Weight = weight;
+        HeightInCentimeters = heightInCentimeters;
+    }
+}
+
+public enum Gender
+{
+    Male,
+    Female,
+    Other
+}
